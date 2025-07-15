a48ff6f13e3ed73710f8629453c3804991ac0f39
ShubhamBhavsar101 <Shubhamcbhavsar198@gmail.com>
2025-07-15 14:28:22 +0530

    Added Scripts

---
+++ Scripts/aws_resource_tracker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+################################################
+# Author: Shubham Bhavsar
+# Date: 15th July 2025
+# Version: v1
+# 
+# Description:
+# This script will report the AWS resource usage
+################################################
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+set -x
+
+# List S3 Buckets
+aws s3 ls
+
+# List EC2 Instances
+aws ec2 describe-instances
+
+# List AWS Lambda functions
+aws lambda list-functions
+
+# List IAM users
+aws iam list-users
